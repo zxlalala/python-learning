61d0dae6d55d81ccbe203164bae7fae5417b9e74
zxlalala <1165347402@qq.com>
2017-01-02 13:48:24 +0800

    second commit

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -5,6 +5,7 @@ from flask import Flask, request, url_for, render_template
 app = Flask(__name__)
 
 #路由
+#dhukjsiuofj
 @app.route('/')
 def hello_world():
     return render_template('index.html') #渲染模板
