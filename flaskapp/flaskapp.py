d851e78ecefe5b17ebe8ccea94ccdb4f5d5ae8f5
zxlalala <1165347402@qq.com>
2017-01-02 13:42:33 +0800

    first commit

---
+++ flaskapp/flaskapp.py
@@ -0,0 +1,33 @@
+#coding= utf-8
+from flask import Flask, request, url_for, render_template
+
+#实例化一个flask对象
+app = Flask(__name__)
+
+#路由
+@app.route('/')
+def hello_world():
+    return render_template('index.html') #渲染模板
+
+@app.route('/user')
+def hello_user():
+    return 'Hello User!'
+
+@app.route('/user/<id>')
+def user_id( id ):
+    return 'hello user'+ id
+
+@app.route('/query_user')
+def query_user():
+    id=request.args.get('id')
+    return 'hello user'+ id
+
+@app.route('/query_url')
+def query_url():
+    return 'query url:'+url_for('query_user')
+
+
+
+# 默认路径为localhost，端口为5000
+if __name__ == '__main__':
+    app.run()
